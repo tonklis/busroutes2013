3145c2f740d3999e540ce3b8dee8bdf39efb769f
tonklis <benjamin.hernandez@in2teck.com>
2013-02-26 00:58:02 -0600

    init

---
+++ config.ru
@@ -0,0 +1,4 @@
+# This file is used by Rack-based servers to start the application.
+
+require ::File.expand_path('../config/environment',  __FILE__)
+run Busroutes2013::Application
---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Busroutes2013::Application.config.secret_token = '38c0b08c8289868276d279ee0dcba19c04a61a7efa132bc85bbe18a50206823d3c1ef910f2e1052661467891ce00f32fb921891b5295fd211f99f577dbd67dda'
